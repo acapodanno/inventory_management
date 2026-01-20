8c4818c346e357b839e07d6741ea298fe9ee66e3
acapodanno <alessandro.capodanno95@gmail.com>
2026-01-20 13:31:36 +0100

    add. .

--- util/generate_excel.py
+++ util/generate_excel.py
@@ -8,11 +8,11 @@ def generate_excel(data, file_path):
 
     headers_daily = {0: "numberOrders", 1: "numberOrderCompleted", 2: "numberOrderPending"}
     for col, header in enumerate(headers_daily):
-        sheet_daily_report.write(0, col, header)
+        sheet_daily_report.write(0, col, headers_daily[header])
 
     headers_product_most_fulfilled = {0: "productCode", 1: "quantityFulfilled"}
     for col, header in enumerate(headers_product_most_fulfilled):
-        sheet_product_most_fulfilled.write(0, col, header)
+        sheet_product_most_fulfilled.write(0, col, headers_product_most_fulfilled[header])
     for row_idx, row_data in enumerate(data["productsMostFulfilled"], start=1):
         for col_idx, cell_data in enumerate(row_data):
             sheet_product_most_fulfilled.write(row_idx, col_idx, cell_data)
